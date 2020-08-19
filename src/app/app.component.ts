82604bfaa0d62e5d452afed3a398757de19d9e0d
annetheblobby <anneliu1999@outlook.com>
2020-08-18 22:12:03 -0400

    page transition and width of content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as Feather from 'feather-icons';
+import { Router, RouterEvent, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { fadeAnimation } from './animations/fade.animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeAnimation]
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'personalSite';
-  ngAfterViewInit() {
-    Feather.replace();
+
+  constructor() { }
+
+  public getRouterOutletState(outlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
   }
+
 }
   
