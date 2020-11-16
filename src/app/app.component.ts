9e1d28373c0b03299a9b8ddf8d5b4a56abe74f73
annetheblobby <anneliu1999@outlook.com>
2020-11-16 00:03:15 -0500

    mobile changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
 
   ngOnInit(){
     AOS.init({
-      duration: 1200,
+      duration: 1400,
     })
 
     AOS.init({
@@ -25,7 +25,6 @@ export class AppComponent {
       easing: 'ease', // default easing for AOS animations
       once: true, // whether animation should happen only once - while scrolling down
       mirror: false, // whether elements should animate out while scrolling past them
-      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
     
     });
   }
